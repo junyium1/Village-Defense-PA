1123b85679498a1600b43239ca489f4ad91941f4
junyium1 <elkihal.aziz@icloud.com>
2026-06-13 01:15:10 +0200

    feat: added share twitter button

--- Assets/Scripts/Menus/ShareButton.cs
+++ Assets/Scripts/Menus/ShareButton.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class ShareButton : MonoBehaviour
 {
-    private IEnumerator screenNshare()
+    private string _sharemessage;
+
+    public void ClickShareButton()
+    {
+        _sharemessage = "12000 bees waiting in front of your house have a message for you : Come outside."; // ajouter "+ varscore" pour output le score obtenu. 
+        StartCoroutine(ScreenNshare());
+    }
+    
+    
+    private IEnumerator ScreenNshare()
     {
         yield return new WaitForEndOfFrame();
         
@@ -12,12 +21,21 @@ public class ShareButton : MonoBehaviour
         ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         ss.Apply();
         
-        string filePath = Path.Combine(Application.streamingAssetsPath, "shareimage.png");
+        string filePath = Path.Combine(Application.persistentDataPath, "shareimage.png").Replace("/", "\\");
         File.WriteAllBytes(filePath, ss.EncodeToPNG());
         
         Destroy(ss);
         
-        new NativeShare().AddFile(filePath).SetSubject("VillageDefender").SetText("Kill yourself").Share();
+        #if UNITY_STANDALONE_WIN
+        var psi = new System.Diagnostics.ProcessStartInfo()
+        {
+            FileName = "explorer.exe",
+            Arguments = $"/select,\"{filePath}\""
+        };
+        System.Diagnostics.Process.Start(psi);
+        
+        string encoded = System.Uri.EscapeDataString(_sharemessage); 
+        Application.OpenURL($"https://twitter.com/intent/tweet?text={encoded}");
+        #endif
     }
-    
 }
